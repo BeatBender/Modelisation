b4fa416c66ef4a31eaab73f1b1edddadb60180c0
willbur415 <dery.william@hotmail.com>
2018-04-24 22:55:11 -0400

    added observer skeleton

---
+++ TrashInc/TrashInc/Observable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrashInc;
+
+namespace TrashInc
+{
+    interface Observable
+    {
+        void addObserver(Obverser observer);
+        void removeObserver(Obverser observer);
+        void notifyObservers();
+    }
+}
