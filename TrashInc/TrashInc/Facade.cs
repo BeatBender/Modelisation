07c20fa2f85eaf99688dbfc37dd66bd538022062
willbur415 <dery.william@hotmail.com>
2018-04-24 23:00:16 -0400

    Added facade and prototype skeletons

---
+++ TrashInc/TrashInc/Facade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrashInc
+{
+    class Facade
+    {
+        public void GetRoute() { }
+        public void ActualiseRoute() { }
+    }
+}
